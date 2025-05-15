83bf83348cf954ad03a04cf4ca48969de0452d06
jennyo <jennifer.oleary.10@gmail.com>
2025-05-14 23:58:42 -0500

    start plan

--- movie-react-app/src/components/selector.jsx
+++ movie-react-app/src/components/selector.jsx
@@ -1,5 +1,30 @@
-const MovieSelector = () => {
 
+function MovieSelector() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        // Simulate an API call
+        setTimeout(() => {
+            setMovie("Sunny, 75°F");
+            setIsLoading(false);
+        }, 2000);
+    }, []);
+
+    return (
+        <div>
+            <div>
+                {isLoading ? <p>Loading movies...</p> : <p>Top Movie: {movie}</p>}
+            </div>
+            <div id="genre-dropdown">
+                <select>
+                    <option value="select-fantasy">fantasy</option>
+                    <option value="select-action">action</option>
+                    <option value="select-sci-fi">sci-fi</option>
+                </select>
+            </div>
+        </div>
+    );
 }
 
 export default MovieSelector;
